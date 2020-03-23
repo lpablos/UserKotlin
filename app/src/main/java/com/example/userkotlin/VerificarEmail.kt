bb9827588e537db2a53c5216e8ef9e1de4ca3885
lpablo <gevolutionxalapa>
2020-03-23 00:08:53 -0600

    Generando vistas y bases del proyecto

---
+++ app/src/main/java/com/example/userkotlin/VerificarEmail.kt
@@ -0,0 +1,12 @@
+package com.example.userkotlin
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VerificarEmail : AppCompatActivity (){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.verificar_email)
+    }
+}
